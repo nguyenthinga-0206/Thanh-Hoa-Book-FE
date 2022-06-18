64ba0a9daac1d8e1ea2975cea26803ba3ff8e55a
nguyenthinga-0206 <nga3421@gmail.com>
2022-06-18 12:32:04 +0700

    ng build -> file dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
   production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyD7ikTz5y2bKw4mKYYn5ZEASkIz7Kg-gdg",
+    authDomain: "honnya-img.firebaseapp.com",
+    databaseURL: "https://honnya-img-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "honnya-img",
+    storageBucket: "honnya-img.appspot.com",
+    messagingSenderId: "266973106524",
+    appId: "1:266973106524:web:ecb11dac3c5b2d62e0f199",
+    measurementId: "G-NLD38WWXTZ"
+  }
 };
+
